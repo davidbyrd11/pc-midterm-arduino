ef590ae1f5476b2334a76e81d5139b1823152734
Shohan <shohan.hasan.x6@gmail.com>
2013-03-26 13:33:49 -0400

    More arrays and removed dead code.

--- pc_midterm_arduino.ino
+++ pc_midterm_arduino.ino
@@ -1,33 +1,26 @@
+/* Turns the Arduino into a signal processor for several capacitive sensors.
+   Maintains a state for each sensor and writes changed states to Serial port. */
 #include <CapacitiveSensor.h>
 
-/*
- * CapitiveSense Library Demo Sketch
- * Paul Badger 2008
- * Uses a high value resistor e.g. 10M between send pin and receive pin
- * Resistor effects sensitivity, experiment with values, 50K - 50M. Larger resistor values yield larger sensor values.
- * Receive pin is the sensor pin - try different amounts of foil/metal on this pin
- */
-
 const int NUM_SENSORS = 5;
+int recPins[NUM_SENSORS] = {4,13,8,0,6};
+int sensePins[NUM_SENSORS] = {2,12,9,1,7};
 int SAMPLES = 30;
 const int AUTOCAL = 0xFFFFFFFF;
-int THRESHOLDS[NUM_SENSORS] = {200,200,200,200,200};
+const int DEFAULT_THRESH = 200;
+int THRESHOLDS[NUM_SENSORS];// = {200,200,200,200,200};
 CapacitiveSensor *sensors[NUM_SENSORS];
 int states[NUM_SENSORS]; // keeps track of the state of each sensor as an int code: 0=off, 1=on
 
-//CapacitiveSensor   cs1 = CapacitiveSensor(4,2);        // 10M resistor between pins 4 & 2, pin 2 is sensor pin, add a wire and or foil if desired
-//CapacitiveSensor   cs2 = CapacitiveSensor(13,12);        // 10M resistor between pins 4 & 6, pin 6 is sensor pin, add a wire and or foil
 void setup() {
-  sensors[0] = new CapacitiveSensor(4,2);
-  sensors[0]->set_CS_AutocaL_Millis(AUTOCAL);
-  sensors[1] = new CapacitiveSensor(13,12);
-  sensors[1]->set_CS_AutocaL_Millis(AUTOCAL);
-  sensors[2] = new CapacitiveSensor(8,9);
-  sensors[2]->set_CS_AutocaL_Millis(AUTOCAL);
-  sensors[3] = new CapacitiveSensor(0,1);
-  sensors[3]->set_CS_AutocaL_Millis(AUTOCAL);
-  sensor[4] = new CapacitiveSensor(6,7);
-  sensors[4]->set_CS_AutocaL_Millis(AUTOCAL);
+  
+  /* Instantiate a cap. sensor for each of the sensors and set its autocal value.
+      Also sets the threshold to default value. */
+  for(int i=0; i<NUM_SENSORS; i++){
+    sensors[i] = new CapacitiveSensor(recPins[i],sensePins[i]);
+    sensors[i]->set_CS_AutocaL_Millis(AUTOCAL);
+    THRESHOLDS[i] = DEFAULT_THRESH;
+  }
   Serial.begin(9600); 
 }
 
@@ -54,15 +47,5 @@ void loop() {
       }
     }
     //Serial.println(); //DEBUG
-    
-    /*
-    long total1 =  cs1.capacitiveSensor(30); //TODO (david) wtf is the 30 for?
-    long total2 =  cs2.capacitiveSensor(30);
-
-    Serial.print(total1);
-    Serial.print(",");
-    Serial.print(total2);
-    Serial.print("\n");
-    */
     delay(25);
 }
